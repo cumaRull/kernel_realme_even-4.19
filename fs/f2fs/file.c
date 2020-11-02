d69ab16e1587c40b14d848c6f8f55051f5356f1a
Chao Yu <yuchao0@huawei.com>
2020-11-02 17:36:58 +0800

    f2fs: fix to seek incorrect data offset in inline data file
    As kitestramuort reported:
    
    F2FS-fs (nvme0n1p4): access invalid blkaddr:1598541474
    [   25.725898] ------------[ cut here ]------------
    [   25.725903] WARNING: CPU: 6 PID: 2018 at f2fs_is_valid_blkaddr+0x23a/0x250
    [   25.725923] Call Trace:
    [   25.725927]  ? f2fs_llseek+0x204/0x620
    [   25.725929]  ? ovl_copy_up_data+0x14f/0x200
    [   25.725931]  ? ovl_copy_up_inode+0x174/0x1e0
    [   25.725933]  ? ovl_copy_up_one+0xa22/0xdf0
    [   25.725936]  ? ovl_copy_up_flags+0xa6/0xf0
    [   25.725938]  ? ovl_aio_cleanup_handler+0xd0/0xd0
    [   25.725939]  ? ovl_maybe_copy_up+0x86/0xa0
    [   25.725941]  ? ovl_open+0x22/0x80
    [   25.725943]  ? do_dentry_open+0x136/0x350
    [   25.725945]  ? path_openat+0xb7e/0xf40
    [   25.725947]  ? __check_sticky+0x40/0x40
    [   25.725948]  ? do_filp_open+0x70/0x100
    [   25.725950]  ? __check_sticky+0x40/0x40
    [   25.725951]  ? __check_sticky+0x40/0x40
    [   25.725953]  ? __x64_sys_openat+0x1db/0x2c0
    [   25.725955]  ? do_syscall_64+0x2d/0x40
    [   25.725957]  ? entry_SYSCALL_64_after_hwframe+0x44/0xa9
    
    llseek() reports invalid block address access, the root cause is if
    file has inline data, f2fs_seek_block() will access inline data regard
    as block address index in inode block, which should be wrong, fix it.
    
    Mot-CRs-fixed: (CR)
    
    Change-Id: Ie43164d5d4b25f46ab857057342ab3748ffd8fdd
    Reported-by: kitestramuort <kitestramuort@autistici.org>
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Reviewed-on: https://gerrit.mot.com/2020032
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Qiong Xie <xieq2@motorola.com>
    Submit-Approved: Jira Key
    Signed-off-by: wulan17 <wulan17@komodos.id>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -446,10 +446,14 @@ static loff_t f2fs_seek_block(struct file *file, loff_t offset, int whence)
 		goto fail;
 
 	/* handle inline data case */
-	if (f2fs_has_inline_data(inode) || f2fs_has_inline_dentry(inode)) {
-		if (whence == SEEK_HOLE)
+	if (f2fs_has_inline_data(inode)) {
+		if (whence == SEEK_HOLE) {
 			data_ofs = isize;
-		goto found;
+			goto found;
+		} else if (whence == SEEK_DATA) {
+			data_ofs = offset;
+			goto found;
+		}
 	}
 
 	pgofs = (pgoff_t)(offset >> PAGE_SHIFT);
