e9060fc77933c47b5f692cb174aa3e7ce5c7c08f
nil <shas45558@gmail.com>
2021-11-27 01:51:10 -0500

    Add shas-dream cpu govt

--- drivers/misc/mediatek/base/power/upower_v2/mtk_unified_power.c
+++ drivers/misc/mediatek/base/power/upower_v2/mtk_unified_power.c
@@ -339,8 +339,9 @@ static void upower_init_rownum(void)
 
 static unsigned int eem_is_enabled(void)
 {
-/* #ifndef EARLY_PORTING_EEM */
-	return mt_eem_is_enabled();
+#ifndef EEM_DISABLE
+	return 0;
+#endif
 }
 
 static void upower_wait_for_eem_volt_done(void)
